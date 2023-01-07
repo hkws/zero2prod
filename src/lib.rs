c4223dfb7947316abaec8d85cc60ca52d7e6b3c2
hkws <hkws0130@gmail.com>
2023-01-08 00:44:37 +0900

    feat: add empty subscriptions api

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,28 @@
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
+use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> std::io::Result<Server> {
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub fn run(listener: TcpListener) -> std::io::Result<Server> {
     let server = HttpServer::new(|| {
                                 App::new()
                                     .route("/health_check", web::get().to(health_check))
+                                    .route("/subscriptions", web::post().to(subscribe))
                             })
-                            .bind("127.0.0.1:8000")?
+                            .listen(listener)?
                             .run();
     Ok(server)
 }
